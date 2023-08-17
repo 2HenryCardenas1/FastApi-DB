ae38c336580318bfc9868cc23013cdb17b6a2074
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-08-17 15:58:41 -0500

    conection to sqlite

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
 
+from config.database import base, engine, session
+from models.movie import Movie
+
 app = FastAPI()
 
 app.title = "Fast Api whit Data base"
 app.description = "This is a very fancy project, with auto docs for the API and everything"
 app.version = "0.0.1"
+
+
+# Create all tables in the database
+base.metadata.create_all(bind=engine)
 
 
 
